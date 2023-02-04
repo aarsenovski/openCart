4e008ac5ca9cdef939afc8623567e9a56573cf2b
Andrej Arsenovski <andrejarsenovski@gmail.com>
2023-02-04 22:21:38 +0100

    feat: add mochawesome reporter (#4)

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
 
-    baseUrl: "http://opencart.abstracta.us/",
+    baseUrl: 'http://opencart.abstracta.us/',
     defaultCommandTimeout: 5000,
   },
-});
+})
