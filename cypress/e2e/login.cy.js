2f5c7a9c56fb3b4d6d7123d4d544ff4256ac05d4
Andrej Arsenovski <andrejarsenovski@gmail.com>
2023-02-05 11:54:32 +0100

    feat: add cy.ssession (#6)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,8 @@
-import { link } from '../pages/link' //named export
-import { fillLoginForm, loginLocators } from '../pages/login'
 import user from '../fixtures/userRegister.json'
+import loginPage from '../pages/login'
 
 describe('test login functionality', () => {
   it('should login an existing user', () => {
-    cy.visit(`/${link.login}`)
-
-    fillLoginForm(user)
-    cy.get(loginLocators.login).click()
-    cy.url().should('contain', 'account/account')
+    loginPage.login(user)
   })
 })
--- cypress/pages/login.js
+++ cypress/pages/login.js
@@ -1,10 +1,31 @@
+import user from '../fixtures/userRegister.json'
+import { link } from './link'
+
 export const loginLocators = {
   email: '#input-email',
   password: '#input-password',
   login: '[value="Login"]',
 }
 
-export const fillLoginForm = (user) => {
-  cy.get(loginLocators.email).type(user.email)
-  cy.get(loginLocators.password).type(user.password)
+export const login = (user) => {
+  cy.session(
+    [user.firstName, user.password],
+    () => {
+      cy.visit(`/${link.login}`)
+      cy.get(loginLocators.email).type(user.email)
+      cy.get(loginLocators.password).type(user.password)
+      cy.get(loginLocators.login).click()
+    },
+    {
+      validate() {
+        cy.getCookie('OCSESSID').should('exist')
+      },
+    },
+  )
 }
+const loginPage = {
+  loginLocators,
+  login,
+}
+
+export default loginPage
