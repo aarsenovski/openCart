949fb46a1ebef6254b8b1eee58abdcc7224efefb
Andrej Arsenovski <andrejarsenovski@gmail.com>
2023-04-11 13:18:39 +0000

    refactor and update tests (#7)

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -4,12 +4,12 @@ import { link } from '../pages/link'
 describe('test register functionality', () => {
   it('should register a new user', () => {
     cy.visit(`/${link.register}`)
-
     cy.fixture('userRegister').then((user) => {
       const newUser = {
         ...user,
         email: `cypress-test${Date.now()}@test.com`,
       }
+
       registerFillForm(newUser)
     })
     cy.get(registerPageLocators.policyTickBox).click()
