8cdb29e8b6bce30c60fab47931fb77c90c694c62
Andrej Arsenovski <andrejarsenovski@gmail.com>
2023-05-01 15:31:42 +0200

    feat: switch to esbuild preprocessor (#9)

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require('cypress')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
+    video: false,
     setupNodeEvents(on, config) {
+      on('file:preprocessor', createBundler())
       require('cypress-mochawesome-reporter/plugin')(on)
     },
 
