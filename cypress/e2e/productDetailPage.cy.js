8ce03c6615b6f19f04be87d6bc6af0bb2bf32144
Andrej Arsenovski <andrejarsenovski@gmail.com>
2023-07-04 16:30:09 +0200

    Create cypress workflow

---
+++ cypress/e2e/productDetailPage.cy.js
@@ -0,0 +1,37 @@
+import product from '../fixtures/product.json'
+import user from '../fixtures/userRegister.json'
+import { link } from '../pages/link'
+import loginPage from '../pages/login'
+import { productPagelocators } from '../pages/productPage'
+import { wishListLocators } from '../pages/wishlist'
+
+describe('test productDetailPage functionality', () => {
+  beforeEach(() => {
+    cy.intercept('POST', 'index.php?route=account/wishlist/add').as(
+      'addToWishlist',
+    )
+    cy.intercept('GET', 'index.php?route=account/wishlist').as(
+      'removeFromWishlist',
+    )
+  })
+  it('should add a product to wishlist', () => {
+    loginPage.login(user)
+
+    cy.visit(`index.php?route=product/${product.slug}`)
+    cy.get(productPagelocators.addToWishlist).click()
+    cy.wait('@addToWishlist').then((res) => {
+      expect(res.response.body.total).to.contain('Wish List')
+      expect(res.response.statusCode).to.eq(200)
+    })
+  })
+  it('should delete a product from wishlist', () => {
+    loginPage.login(user)
+
+    cy._addProductToWishlist(40)
+    cy.visit(link.wishlist)
+    cy.get(wishListLocators.removeButton).first().click({ force: true })
+    cy.wait('@removeFromWishlist').then((res) => {
+      expect(res.response.statusCode).to.eq(200)
+    })
+  })
+})
